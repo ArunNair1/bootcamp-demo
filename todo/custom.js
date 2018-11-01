758728bad9ff387b7eb6fa9ab6157a2184709b41
Arun A Nair <arun.nair@cybergindia.local>
2018-11-01 12:34:19 +0530

    toggle on complete button

--- todo/custom.js
+++ todo/custom.js
@@ -18,7 +18,7 @@ function AddNote()
 		let task_count='task_id_'+task_counter+'';
 		let task_content='task_content_'+task_counter+'';
 		//console.log(task_count);
-		$("#tasks_div").append('<div class="container centered-div task-div" style="display:none;" id="'+task_count+'"><div style="margin:0 auto"><h3 id="'+task_content+'" data-value="'+content+'">'+content+'<span class="close-button"><i class="fa fa-times" onclick="DeleteTask('+task_counter+');"></i></span><span class="edit-button" onclick="EditTask('+task_counter+');"><i class="fa fa-edit"></i></span><span class="check-button" id="check_'+task_counter+'" onclick="CompleteTask('+task_counter+');"><i class="fa fa-check"></i></span></h3></div></div>');
+		$("#tasks_div").append('<div class="container centered-div task-div" style="display:none;" id="'+task_count+'"><div style="margin:0 auto"><h3 id="'+task_content+'" data-complete="0" data-value="'+content+'">'+content+'<span class="close-button"><i class="fa fa-times" onclick="DeleteTask('+task_counter+');"></i></span><span class="edit-button" onclick="EditTask('+task_counter+');"><i class="fa fa-edit"></i></span><span class="check-button" id="check_'+task_counter+'" onclick="CompleteTask('+task_counter+');"><i class="fa fa-check"></i></span></h3></div></div>');
 		task_counter++;
 		document.getElementById('task_content').value="";
 		$("#"+task_count).fadeIn(1000);
@@ -65,8 +65,23 @@ function EditTask(task_id)
 
 function CompleteTask(task_id)
 {
+	
 	let task_count='check_'+task_id+'';
-	document.getElementById(task_count).style.color="red";
+	let task_content='task_content_'+task_id+'';
+	if($("#"+task_content).attr("data-complete")==0)
+	{
+		
+		document.getElementById(task_count).style.color="red";
+		document.getElementById(task_content).style.textDecoration ="line-through";
+		$("#"+task_content).attr("data-complete","1");
+	}
+	else
+	{
+			
+		document.getElementById(task_count).style.color="white";
+		document.getElementById(task_content).style.textDecoration ="none";
+		$("#"+task_content).attr("data-complete","0");
+	}
 }
 
 $(document).ready( function(){
